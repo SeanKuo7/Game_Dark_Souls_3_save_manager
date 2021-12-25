adf2df07bf3f74917795dd6f32d8658c2fd1b21f
Sean Bailey <sean.kuo.work@gmail.com>
2021-12-25 10:31:00 +0800

    adding backup function, moving to new branch

--- source/main.py
+++ source/main.py
@@ -1,21 +1,35 @@
 import pathlib
 print("Working directory: ", pathlib.Path().resolve())
 
+def readPathesFromConfig():
+    inputPath = ""
+    outputPath = ""
+    try:
+        print("entered")
+        f = open("config.ini", "r")
+        #print(f.read())
+        dirListRaw = f.readlines()
+        #print(dirList)
+        dirList = []
+        for dir in dirListRaw[:-1]:
+            dirTrimmed = dir[0:len(dir)-1]
+            dirList.append(dirTrimmed)
+        dirList.append(dirListRaw[-1])
+        return(dirList)
+    except:
+        print("reading config.ini failed!!")
+    return
 
-inputPath = ""
-outputPath = ""
-try:
-    print("entered")
-    f = open("config.ini", "r")
-    #print(f.read())
-    dirListRaw = f.readlines()
-    #print(dirList)
-    dirList = []
-    for dir in dirListRaw[:-1]:
-        dirTrimmed = dir[0:len(dir)-1]
-        dirList.append(dirTrimmed)
-    dirList.append(dirListRaw[-1])
-    print(dirList)
-except:
-    print("reading config.ini failed!!")
+def backupSave(inputPath, outputPath):
+    print(inputPath)
+    print(outputPath)
+    
+    return
+
+
+
+if __name__ == "__main__":
+    dirList = readPathesFromConfig()
+    backupSave(dirList[0], dirList[1])
+
 
